cfb952894c5eb61f563d1bcd93544e66068fb212
quyixiao <2621048238@qq.com>
2019-04-17 20:47:09 +0800

    提交测试

---
+++ src/main/java/javavm/JavaVMStackOOM.java
@@ -0,0 +1,27 @@
+package javavm;
+
+public class JavaVMStackOOM {
+
+
+    private void dontStop(){
+        while(true){
+
+        }
+    }
+
+    public void stackLeakByThread(){
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
