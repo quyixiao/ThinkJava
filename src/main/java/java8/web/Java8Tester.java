d8d133f0b7dfe94db00696763bb3ac6e5b724123
quyixiao <2621048238@qq.com>
2019-04-23 10:23:54 +0800

    提交修改

---
+++ src/main/java/java8/web/Java8Tester.java
@@ -0,0 +1,29 @@
+package java8.web;
+
+
+/***
+ * lambda 表达式只能引用标记了 final 的外层局部变量，这就是说不能在lambda 内部修改定义在域外的局部变量，否则会编译错误。
+ */
+public class Java8Tester {
+    final static String salutation = "Hello! ";
+
+    public static void main(String args[]) {
+        GreetingService greetService1 = message ->
+                System.out.println(salutation + message);
+        greetService1.sayMessage("Runoob");
+        //====================相当于下面==============================
+        GreetingService g = new GreetingService() {
+            @Override
+            public void sayMessage(String message) {
+                //salutation = salutation+1;
+                System.out.println(salutation + message);
+            }
+        };
+        g.sayMessage("jack");
+        //===========================================================
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+}
